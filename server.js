ce165ece0d6638f5fe89b2c3f9518f88c50c6b64
duncan-richey <duncan527@gmail.com>
2023-05-02 01:02:15 -0400

    change

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the "src" directory
+app.use(express.static(path.join(__dirname, 'src')));
+
+// Send the "index.html" file for all requests
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+   console.log(`Server is running on port ${port}`);
+});
